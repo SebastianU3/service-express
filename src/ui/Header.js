afe03e1fecd3becc4efc13c08e1fd5aa3a5e63b2
TBwze <darreniskandar27@gmail.com>
2023-11-18 18:41:56 +0700

    header navbar

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <a href="#"><img src="#"></img></a>
+      <ul>
+        <li><a href="#"><img src="#"></img></a></li>
+        <li><a href="#"><img src="#"></img></a></li>
+        <li>
+          <div class="Account">
+            <ul>
+              <li><img src="#"></img></li>
+              <li><h4>Account</h4></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header;
